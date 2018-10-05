0a7ee1595707d91887185f40dda7e6b908b41d3f
Paul <paul@modus.nl>
2018-10-05 08:48:55 +0200

    script for uploading local data to Data Lake cluster

---
+++ Data/Upload-ADLS.ps1
@@ -0,0 +1,11 @@
+﻿# Initialize variables
+$LocalPath = $PSScriptRoot
+$RemotePath = '/GLEIF'
+$ADLSAccountName = "modusadls"
+
+# Iterate through local Zip & Xml files and upload to DataLake
+$items = Get-ChildItem -Path "$LocalPath\*" -Include *zip, *.xml -Filter *lei2*
+foreach($item in $items){
+    $destination = "$RemotePath/$($item.Name)"
+    Import-AzureRmDataLakeStoreItem -AccountName $ADLSAccountName -Path $item.FullName -Destination $destination -Force
+}
