0bd1719c019dc5aa120b68e93cbd0ae06a713b22
Paul <paul@modus.nl>
2018-10-09 06:58:52 +0200

    extending documentation

--- Database/Gleif.1.OpenRowset.Xml.sql
+++ Database/Gleif.1.OpenRowset.Xml.sql
@@ -10,6 +10,11 @@ Top10000 = 22Mb
 https://docs.microsoft.com/en-us/sql/t-sql/functions/openxml-transact-sql?view=sql-server-2017
 OPENXML() --> needs to prepare whole XML Document: EXEC sp_xml_preparedocument @idoc OUTPUT, @doc;  
 Therefore use OPENROWSET() instead!
+
+OPENROWSET() LOB types;
+	SINGLE_BLOB		-> reads a file as varbinary(max)
+	SINGLE_CLOB		-> reads a file as varchar(max)
+	SINGLE_NCLOB	-> reads a file as nvarchar(max)
 */
 
 -- 1.
@@ -18,9 +23,6 @@ SELECT CONVERT(XML, rs.BulkColumn) AS XmlDocument
 FROM OPENROWSET(
 	BULK 'C:\Users\Paul\Source\Repos\GLEIF\Data\20180901-gleif-concatenated-file-lei2-Top100.xml', 
 	SINGLE_BLOB
-	--SINGLE_BLOB, which reads a file as varbinary(max)
-	--SINGLE_CLOB, which reads a file as varchar(max)
-	--SINGLE_NCLOB, which reads a file as nvarchar(max)
 ) AS rs;    
 
 
